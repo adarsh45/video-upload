b050e402484f225b27d1ea18480c72d8ab23b3c4
Adarsh Shete <shete.adarsh@gmail.com>
2023-04-18 23:53:15 +0530

    plate nnumbers handled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
+  const [detectedPlates, setDetectedPlates] = useState([]);
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
@@ -17,14 +19,13 @@ function App() {
     formData.append("video", selectedFile);
 
     // console.log(selectedFile);
-    fetch("https://9dca-103-82-43-56.ngrok-free.app/uploadvideo", {
-      method: "POST",
-      body: formData,
-    })
+    axios
+      .post("https://9dca-103-82-43-56.ngrok-free.app/uploadvideo", formData)
       .then((response) => {
         // handle response
-        console.log(response.status);
+        console.log(response);
         if (response.status === 200) {
+          setDetectedPlates(response?.data?.plates);
           alert("Success!");
         } else {
           alert("Something went wrong!");
@@ -43,24 +44,47 @@ function App() {
 
   return (
     <div className="container">
-      <label htmlFor="videos" className="drop-container">
-        <span className="drop-title">Drop files here</span>
-        or
-        <input
-          type="file"
-          id="vidoes"
-          accept="*/*"
-          required
-          onChange={handleFileSelect}
-        />
-      </label>
-      {isUploading ? (
-        <div className="loader"></div>
-      ) : (
-        <button style={{ marginTop: "1rem" }} onClick={handleUpload}>
-          Upload
-        </button>
-      )}
+      <div>
+        <label htmlFor="videos" className="drop-container">
+          <span className="drop-title">Drop files here</span>
+          or
+          <input
+            type="file"
+            id="vidoes"
+            accept="*/*"
+            required
+            onChange={handleFileSelect}
+          />
+        </label>
+        <div
+          style={{
+            margin: "auto",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          {isUploading ? (
+            <div className="loader"></div>
+          ) : (
+            <button style={{ marginTop: "1rem" }} onClick={handleUpload}>
+              Upload
+            </button>
+          )}
+          <div>
+            <button style={{ marginTop: "1rem" }}>Detect Camera</button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        {detectedPlates.length
+          ? detectedPlates.map((plateNumber) => (
+              <div className="plate" key={plateNumber}>
+                {plateNumber}
+              </div>
+            ))
+          : ""}
+      </div>
     </div>
   );
 }
