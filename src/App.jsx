3a3f76e81d13bfa919258cfb3db08ae19999cfdc
Adarsh Shete <shete.adarsh@gmail.com>
2023-04-18 22:39:09 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleFileSelect = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    if (!selectedFile) return alert("Please choose file first!");
+    setIsUploading(true);
+
+    const formData = new FormData();
+    formData.append("video", selectedFile);
+
+    // console.log(selectedFile);
+    fetch("/upload-video", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        // handle response
+        console.log(response.status);
+        if (response.status === 200) {
+          alert("Success!");
+        } else {
+          alert("Something went wrong!");
+        }
+      })
+      .catch((error) => {
+        // handle error
+        alert("Something went wrong!");
+        console.error(error);
+      })
+      .finally(() => {
+        setIsUploading(false);
+        setSelectedFile(null);
+      });
+  };
+
+  return (
+    <div className="container">
+      <label htmlFor="videos" className="drop-container">
+        <span className="drop-title">Drop files here</span>
+        or
+        <input
+          type="file"
+          id="vidoes"
+          accept="image/*"
+          required
+          onChange={handleFileSelect}
+        />
+      </label>
+      {isUploading ? (
+        <div className="loader"></div>
+      ) : (
+        <button style={{ marginTop: "1rem" }} onClick={handleUpload}>
+          Upload
+        </button>
+      )}
+    </div>
+  );
+}
+
+export default App;
